1971f0162c33695370f7b4da3d433533aca4a911
LeslieLiuZJNU <LeslieLiuZJNU@foxmail.com>
2021-07-10 17:35:59 +0800

    Update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__)
 
 
-def find_student_execute(name):
+def find_student_class(name):
     http_client = None
     try:
         http_client = http.client.HTTPConnection('10.1.47.99:8000')
@@ -25,6 +25,23 @@ def find_student_execute(name):
             http_client.close()
 
 
+def find_student_picture(id):
+    http_client = None
+    try:
+        http_client = http.client.HTTPConnection('10.1.47.99:8000')
+        http_client.request('GET', '/dataservice/api/1/165?stu_id=' + urllib_request.pathname2url(id))
+        response = http_client.getresponse()
+        json_string = response.read().decode()
+        json_object = json.loads(json_string)
+        student_picture = ("http://data.ourschool.cn:180/dmp/" + json_object['data']['rows'][0]['photo']).strip(',')
+        return student_picture
+    except Exception as e:
+        return e
+    finally:
+        if http_client:
+            http_client.close()
+
+
 @app.route('/FindStudentSubmit')
 def find_student_submit():
     return render_template('find_student_submit.html')
@@ -34,9 +51,11 @@ def find_student_submit():
 def find_student_result():
     student_name = request.form.get('student_name')
     if student_name != '':
-        student_json_list = find_student_execute(student_name)
+        student_json_list = find_student_class(student_name)
         if len(student_json_list) != 0:
-            return render_template('find_student_result.html', list=student_json_list)
+            student_id = student_json_list[0]['school_card_number']
+            student_picture = find_student_picture(student_id)
+            return render_template('find_student_result.html', list=student_json_list, picture=student_picture)
         else:
             return '未找到此人'
     else:
@@ -49,15 +68,4 @@ def data_display():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-#f=open()
-#json.loads()[data]
-
-#@app.route('/Submit')
-#def sub():
-
-
-#@app.route('/Display')
-#def dis():
+    app.run(host='0.0.0.0')
